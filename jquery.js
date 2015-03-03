5068c312e0f3cd543ef7f7cf6e51a366456633dd
Clyde G Marl <marl.clyde@gmail.com>
2015-03-03 14:23:25 -0800

    add third 'colorful' theme, three separate buttons, and clean up JS code using object.

--- jquery.js
+++ jquery.js
@@ -1,12 +1,6 @@
 $(document).ready(function() {
   $('footer').on('click','button',function() {
-    $('body').toggleClass('swapper');
-    $('.jumbotron').toggleClass('swapper');
-    var current = $(this).text();
-    if (current == 'Dark Theme'){
-      $(this).text('Light Theme')
-    } else {
-      $(this).text('Dark Theme')
-    }
+    clicked = $(this).data();
+    $('body').removeClass().addClass(clicked.theme);
   });
 });
