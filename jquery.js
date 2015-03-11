f2044289c6005ca240d3c7e39b2e2059f3a3ca8e
Clyde G Marl <marl.clyde@gmail.com>
2015-03-11 15:27:35 -0700

    clean up stylesheet and javascript code

--- jquery.js
+++ jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('footer').on('click','button',function() {
-    clicked = $(this).data();
+    var clicked = $(this).data();
     $('body').removeClass().addClass(clicked.theme);
   });
 });
